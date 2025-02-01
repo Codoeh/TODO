87c024046439358adb4b78e59dae61e362f27bcf
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-02-01 12:15:34 +0100

    add view for homepage

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from catalog.models import Task, Tag
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
