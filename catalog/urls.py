763a7676e81634cdc814ef5da5f62c690f64cabe
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-02-01 20:38:28 +0100

    add task delete view

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,7 @@ from .views import (
     TaskListView,
     TaskCreateView,
     TaskUpdateView,
+    TaskDeleteView,
     TagListView,
 )
 
@@ -12,6 +13,7 @@ urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "catalog"
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from catalog.forms import TaskForm
 from catalog.models import Task, Tag
@@ -20,6 +20,11 @@ class TaskCreateView(CreateView):
 class TaskUpdateView(UpdateView):
     model = Task
     form_class = TaskForm
+    success_url = reverse_lazy("catalog:task-list")
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
     success_url = reverse_lazy("catalog:task-list")
 
 
