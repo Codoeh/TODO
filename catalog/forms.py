b4e89a5f2f866ada7daee0b651cb2aedbc8f836c
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2025-02-01 21:02:19 +0100

    add form for tag

--- catalog/forms.py
+++ catalog/forms.py
@@ -14,3 +14,9 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["content", "deadline", "tags", "is_done",]
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ["name"]
